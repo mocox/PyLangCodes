c2f8e695390aebba8ad9b36d2a3cfa3bca87988c
mo_cox <mo_cox@msn.com>
2024-09-23 18:01:08 +0100

    updated

--- Spanish.py
+++ Spanish.py
@@ -1,16 +1,14 @@
 import tkinter as tk
 
 
-class GermanLanguage:
+class SpanishLanguage:
     def __init__(self, root: tk.Tk()):
-        self.root: tk.Tk = root
+        self.root = root
 
     def initialise(self):
 
         #self.root.frame = tk.Frame(self.root, height=100, width=100, bd=2, background='#ff0000')
         #self.root.frame.pack()
 
-        label = tk.Label(self.root.frame, text="German", font=('Comic',15), foreground='#00ff00' )
+        label = tk.Label(self.root.frame, text="Spanish", font=('Comic', 15), foreground='#00ff00')
         label.pack()
-
-
--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 import tkinter as tk
 from menu import MainMenu
 from German import GermanLanguage as Gl
+from Spanish import SpanishLanguage as Sl
 
 class MainGui:
     def __init__(self):
-        self.root = ''
-        self.language = "German"
-        self.init()
-
-    def init(self):
         self.root = tk.Tk()
         self.root.title("Python Languages Codes")
         self.root.geometry("800x500")
 
+        self.language = "German"
+        # menu
         MainMenu(self.root, self.menu_clicked)
+        self.init()
+        self.root.mainloop()
+
+    def init(self):
+        # language frame
         if self.language == "German":
-            Gl(self.root)
+            Gl(self.root).initialise()
             print(f"Menu item clicked with parameter: {self.language}")
         elif self.language == "Spanish":
+            Sl(self.root).initialise()
             print(f"Menu item clicked with parameter: {self.language}")
         else:
             print(f"Menu item clicked with parameter: {self.language}")
 
-        self.root.mainloop()
+        self.root.update()
 
     def menu_clicked(self, name: str)->str:
         print(f"Menu item clicked with parameter: {name}")
         self.language = name
-        self.root.destroy()
         self.init()
         return name
 
